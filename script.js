466bb9385fb36b971f33fff3147b8d3b962b9685
Enzo H <haradayasu2010@gmail.com>
2022-06-04 20:22:25 -0300

    Finished rock-paper-scissors game function

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Hello world");
+const state = ["rock", "paper", "scissor"];
+
+function computerPlay(){
+    return state[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection){
+    var player = playerSelection.toLowerCase();
+    var computer = computerSelection.toLowerCase();
+    var results;
+
+
+    if(player == "rock"){
+        if(computer == "paper"){
+            results = "LOSE";
+        }else if(computer == "scissor"){
+            results = "WIN";
+        }else{
+            results = "DRAW";
+        }
+    }else if(player == "paper"){
+        if(computer == "scissor"){
+            results = "LOSE";
+        }else if(computer == "rock"){
+            results = "WIN";
+        }else{
+            results = "DRAW";
+        }
+    }else{
+        if(computer == "rock"){
+            results = "LOSE";
+        }else if(computer == "paper"){
+            results = "WIN";
+        }else{
+            results = "DRAW";
+        }
+    }
+    
+    if(results == "WIN"){
+        return `You ${results}! ${player} beats ${computer}.`
+    }else if(results == "LOSE"){
+        return `You ${results}! ${computer} beats ${player}.`
+    }else{
+        return `${results}!`;
+    }
+}
+
+function game(times){
+    for(let i = 0; i < times; i++){
+        let player = prompt("Rock, Paper or Scissors?: ");
+        let computer = computerPlay();
+        console.log(playRound(player, computer));
+    }
+}
